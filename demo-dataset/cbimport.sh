b5e1898dcae37a4fb82942c3e7e09ef9e2a821e2
Jacob Wood <jake_wood@mac.com>
2026-04-21 20:06:59 -0700

    add demo dataset and cbimport commands required to load data for each store into the appropriate target collections.

---
+++ demo-dataset/cbimport.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# AA-Store Data
+cbimport json --format list --cluster couchbases://{Connection String} --username {Username} --password {Password} --bucket "supermarket" --scope-collection-exp "AA-Store.inventory" --dataset 'file://./aa_store_inventory.json' --generate-key "%id%"
+cbimport json --format list --cluster couchbases://{Connection String} --username {Username} --password {Password} --bucket "supermarket" --scope-collection-exp "AA-Store.profile" --dataset 'file://./aa-store-01-profile.json' --generate-key "%id%"
+
+# NYC-Store Data
+cbimport json --format list --cluster couchbases://{Connection String} --username {Username} --password {Password} --bucket "supermarket" --scope-collection-exp "NYC-Store.inventory" --dataset 'file://./nyc_store_inventory.json' --generate-key "%id%"
+cbimport json --format list --cluster couchbases://{Connection String} --username {Username} --password {Password} --bucket "supermarket" --scope-collection-exp "NYC-Store.profile" --dataset 'file://./nyc-store-01-profile.json' --generate-key "%id%"
